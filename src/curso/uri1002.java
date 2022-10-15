85b4ab787f33620d55ba5466278ebf580e0a69f4
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-15 12:34:29 -0300

    Seção 3: Estrutura sequencial (URI 1002)

---
+++ src/curso/uri1002.java
@@ -0,0 +1,26 @@
+package curso;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1002 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double n, raio, area;
+        
+        n = 3.14159;
+
+        raio = sc.nextDouble();
+
+        area = n * (raio * raio);
+
+        System.out.printf("A=%.4f%n", area);
+
+        sc.close();
+        
+    }
+    
+}
