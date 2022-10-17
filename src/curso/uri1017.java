08fa7123dc7a93ce9c50d538e26d4b8158bc578a
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-16 22:53:44 -0300

    Seção 3: Estrutura sequencial (URI 1017)

---
+++ src/curso/uri1017.java
@@ -0,0 +1,26 @@
+package curso;
+
+import java.util.Scanner;
+
+public class uri1017 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // int a, b;
+
+        double a, b, c;
+        
+        a = sc.nextInt();
+
+        b = sc.nextInt();
+
+        c = (a * b) / 12;
+
+        System.out.printf("%.3f%n", c);
+
+        sc.close();
+        
+    }
+    
+}
