df0ec88b02c24e7f5757924b5f6b385820553b35
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-16 22:45:44 -0300

    Seção 3: Estrutura sequencial (URI 1016)

---
+++ src/curso/uri1016.java
@@ -0,0 +1,22 @@
+package curso;
+
+import java.util.Scanner;
+
+public class uri1016 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        
+        a = sc.nextInt();
+
+        b = a * 2;
+
+        System.out.println(b + " minutos");
+
+        sc.close();
+        
+    }
+    
+}
