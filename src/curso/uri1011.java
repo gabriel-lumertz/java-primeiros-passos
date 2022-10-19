77a68c8ec6ea3cd97599c66c011dd7c693c2d985
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-18 23:28:51 -0300

    Seção 3: Estrutura sequencial (uri 1011)

---
+++ src/curso/uri1011.java
@@ -0,0 +1,24 @@
+package curso;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1011 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double r = sc.nextDouble();
+
+        double p = 3.14159;
+
+        double v = (4.0 / 3.0) * p * Math.pow(r, 3); 
+
+        System.out.printf("VOLUME = %.3f%n", v);
+
+        sc.close();
+        
+    }
+    
+}
