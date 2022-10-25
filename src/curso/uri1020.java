bfa0238cecd7da78a62e51725ca14db9216139ec
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-25 20:30:23 -0300

    Seção 4: Estrutura condicional (uri 1020)

---
+++ src/curso/uri1020.java
@@ -0,0 +1,32 @@
+package curso;
+
+import java.util.Scanner;
+
+public class uri1020 {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        int n, q, r, v; 
+        
+        n = sc.nextInt();
+
+        r = n;
+
+        v = 365;
+        q = r / v;
+        System.out.println(q + " ano(s)");
+        r = r % v;
+
+        v = 30;
+        q = r / v;
+        System.out.println(q + " mes(s)");
+        r = r % v;
+
+        System.out.println(r + " dia(s)");
+
+        sc.close();
+
+    }
+}
