99dd95f4c80c9c6425225c6649703521bf104bcc
Gabriel Lumertz <glumertz10@gmail.com>
2022-10-31 23:55:36 -0300

    Seção 4: Estrutura condicional (exercicio 1)

--- src/curso/secao4/exercicio1.java
+++ src/curso/secao4/exercicio1.java
@@ -1,12 +1,27 @@
 package curso.secao4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class exercicio1 {
 
     public static void main(String[] args) {
 
-//Todo: resolver exercício aqui:
-        
-        println(
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    double n1 = sc.nextDouble();
+    double n2 = sc.nextDouble();
+
+    double nf = n1 + n2;
+
+    System.out.printf("NOTA FINAL = %.1f%n", nf);
+
+    if (nf < 60.0) {
+        System.out.println("REPROVADO");
+    }
+
+    sc.close();
 
     }
     
