3a2f8d23e44dbb73c5746543bf5ed424f0713d7a
Gabriel Lumertz <glumertz10@gmail.com>
2022-11-01 01:20:46 -0300

    Seção 4: Estrutura condicional (exercicio 3)

---
+++ src/curso/secao4/exercicio3.java
@@ -0,0 +1,34 @@
+package curso.secao4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio3 {
+
+    public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    if (a < b && a < c) {
+        System.out.println("MENOR = " + a);
+    }
+
+    else {
+        if (b < c) {
+            System.out.println("MENOR = " + b);
+        }
+
+        else {
+            System.out.println("MENOR = " + c);
+        }
+    }
+
+    sc.close();
+    
+    }
+}
