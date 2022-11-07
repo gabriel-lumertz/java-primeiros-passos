8d478a6f9fc69a84983e03bef1907b4ac6651d6e
Gabriel Lumertz <glumertz10@gmail.com>
2022-11-06 23:33:53 -0300

    Seção 4: Estrutura condicional (uri1041)

--- src/curso/secao4/uri1041.java
+++ src/curso/secao4/uri1041.java
@@ -9,6 +9,39 @@ public class uri1041 {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+        Double x = sc.nextDouble();
+        Double y = sc.nextDouble();
+
+        if (x == 0.0 && y == 0.0) {
+
+            System.out.println("Origem");
+
+        } else if (x == 0) {
+
+            System.out.println("Eixo Y");
+
+        } else if (y == 0) {
+
+            System.out.println("Eixo X");
+
+        } else if (x > 0 && y > 0) {
+
+            System.out.println("Q1");
+
+        } else if (x < 0 && y > 0) {
+
+            System.out.println("Q2");
+
+        } else if (x < 0 && y < 0) {
+
+            System.out.println("Q3");
+
+        } else if (x > 0 && y < 0) {
+
+            System.out.println("Q4");
+
+        }
 		
 		sc.close();
 	}
